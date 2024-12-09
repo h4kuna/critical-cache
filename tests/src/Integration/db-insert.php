4cc9a8ef0e5d45e38f2ecfb6da0c7a2a35062f7b
Milan Matějček <milan.matejcek@gmail.com>
2024-12-09 09:01:13 +0100

    test(example): create database sqlite with 500 000 unique uuid v4

---
+++ tests/src/Integration/db-insert.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+
+namespace h4kuna\CriticalCache\Tests\Integration;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use h4kuna\CriticalCache\Contracts\RandomGeneratorContract;
+use h4kuna\CriticalCache\Interfaces\UniqueValueServiceInterface;
+use h4kuna\CriticalCache\Services\RandomGenerator;
+use h4kuna\CriticalCache\Services\UniqueValuesGeneratorService;
+use PDO;
+use PDOException;
+
+$database = __DIR__ . '/test.db';
+@unlink($database);
+
+// Připojení k databázi SQLite
+try {
+	$pdo = new PDO('sqlite:' . $database);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	// Vytvoření tabulky, pokud neexistuje
+	$sql = "CREATE TABLE IF NOT EXISTS test (
+                unique_value TEXT NOT NULL
+            )";
+	$pdo->exec($sql);
+
+	$indexSql = "CREATE INDEX IF NOT EXISTS idx_unique_value ON test (unique_value)";
+	$pdo->exec($indexSql);
+
+	echo "Tabulka a index byly vytvořeny nebo již existují.<br>";
+} catch (PDOException $e) {
+	echo "Chyba připojení: " . $e->getMessage();
+}
+
+function placeholder(array $data, string $str): string
+{
+	return substr(str_repeat($str, count($data)), 0, -2);
+}
+
+
+$checker = new class($pdo) implements UniqueValueServiceInterface {
+	private RandomGeneratorContract $randomGenerator;
+
+	public function __construct(private PDO $pdo)
+	{
+		$this->randomGenerator = new RandomGenerator();
+	}
+
+	public function check(array $data): iterable
+	{
+		$sql = sprintf('SELECT unique_value FROM test WHERE unique_value IN (%s)', placeholder($data, '?, '));
+		$stmt = $this->pdo->prepare($sql);
+		$stmt->execute(array_values($data));
+		foreach ($stmt->fetchAll(PDO::FETCH_OBJ) as $row) {
+			yield $row->unique_value;
+		}
+	}
+
+	public function getQueueSize(): int
+	{
+		return 200000;
+	}
+
+	public function getRandomGenerator(): RandomGeneratorContract
+	{
+		return $this->randomGenerator;
+	}
+
+	public function getTries(): ?int
+	{
+		return null;
+	}
+};
+
+$uniqueGenerator = new UniqueValuesGeneratorService();
+
+$limit = floor(500_000 / $checker->getQueueSize());
+$start = microtime(true);
+for ($i = 0; $i < $limit; ++$i) {
+	$values = $uniqueGenerator->execute($checker);
+
+	$sql = "INSERT INTO test (unique_value) VALUES " . placeholder($values, '(?), ');
+
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute($values);
+}
+
+echo microtime(true) - $start;
